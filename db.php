0704d2e2f2115e2225dd24907175c5b8b4fa6a13
valter <valterekholm@hotmail.com>
2021-08-19 02:17:03 +0200

    fixes

--- db.php
+++ db.php
@@ -11,7 +11,6 @@ class db
 	function __construct($conn_vals)
 	{
 		echo("construct");
-		print_r($conn_vals);
 		$this->user = $conn_vals[2];
 		$this->pass = $conn_vals[3];
         $this->db_name = $conn_vals[1];
