352469e1e3155054fdc667b6cc1dc80d2e009280
valter <valterekholm@hotmail.com>
2022-01-20 00:51:06 +0100

    removed hit counter

--- index.php
+++ index.php
@@ -81,10 +81,6 @@ else{
 ?>
 <!--/pre--> <!-- 'pre' disables the word-wrap css function -->
 
-<!-- Start of SimpleHitCounter Code -->
-<div align="center"><a href="http://www.simplehitcounter.com" target="_blank"><img src="http://simplehitcounter.com/hit.php?uid=348135516&f=16777215&b=0" border="0" height="18" width="83" alt="web counter"></a><br><a href="http://www.simplehitcounter.com" target="_blank" style="text-decoration:none;">web counter</a></div>
-<!-- End of SimpleHitCounter Code -->
-
 
 </body>
 </html>
